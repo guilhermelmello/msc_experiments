761935c459bb1a400843a3c94d4c0fd0a7bc4e58
melloGuilherme <guilhermelmello@yahoo.com.br>
2022-11-10 09:37:03 -0300

    Add Assin dataset for STS task.

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -3,6 +3,7 @@ from .loaders import *
 
 _dataset_loaders = {
     'assin_rte': load_assin_rte,
+    'assin_sts': load_assin_sts,
     'assin2_rte': load_assin2_rte
 }
 
@@ -10,7 +11,7 @@ _dataset_loaders = {
 def load_experiment_dataset(dataset_id, **kwargs):
     if dataset_id not in _dataset_loaders:
         raise ValueError(f"Could not find dataset: {dataset_id}")
-    
+
     print(f"Loadind dataset: {dataset_id}")
     dataset_loader = _dataset_loaders[dataset_id]
     data = dataset_loader(**kwargs)
--- src/dataset/loaders.py
+++ src/dataset/loaders.py
@@ -12,6 +12,15 @@ def load_assin_rte(**kwargs):
     return data
 
 
+def load_assin_sts(**kwargs):
+    task = TextPairTemplate(
+        text_column='premise',
+        text_pair_column='hypothesis',
+        label_column='relatedness_score')
+    data = datasets.load_dataset('assin', 'full', task=task)
+    return data
+
+
 def load_assin2_rte(**kwargs):
     task = TextPairTemplate(
         text_column='premise',
