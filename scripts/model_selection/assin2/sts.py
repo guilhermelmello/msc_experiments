fa58526926f2269083f48e39fa6d4026ad95d02d
Guilherme L. Mello <guilhermelmello@yahoo.com.br>
2022-12-02 08:50:51 -0300

    Update comments (results).

--- scripts/model_selection/assin2/sts.py
+++ scripts/model_selection/assin2/sts.py
@@ -12,29 +12,29 @@ run_regression_model_selection(
     dataset_train_split='train',
     dataset_validation_split='validation',
 
-    train_batch_size=128,    # base: 128     large: 64
+    train_batch_size=64,    # base: 128     large: 64
     train_epochs=10,
     train_execs_pre_trial=5,
     train_lr_values=[1e-4, 5e-5, 1e-5, 1e-6, (1e-5, 1e-7), (1e-5, 0)],
 
     model_ids=[
-        'bert-base-cased',                                      # (  ) - 128
-        'bert-base-uncased',                                    # (  ) - 128
-        'roberta-base',                                         # (  ) - 128
-        'bert-base-multilingual-cased',                         # (  ) - 128
-        'neuralmind/bert-base-portuguese-cased',                # (  ) - 128
+        # 'bert-base-cased',                                      # (OK) - 128
+        # 'bert-base-uncased',                                    # (OK) - 128
+        # 'roberta-base',                                         # (OK) - 128
+        # 'bert-base-multilingual-cased',                         # (OK) - 128
+        # 'neuralmind/bert-base-portuguese-cased',                # (OK) - 128
 
-        'bert-large-cased',                                     # (  ) - 128
-        'bert-large-uncased',                                   # (  ) - 128
-        'bert-large-uncased-whole-word-masking',                # (  ) - 128
-        'bert-large-cased-whole-word-masking',                  # (  ) - 128
-        'roberta-large',                                        # (  ) - 128
-        'xlm-mlm-17-1280',                                      # (  ) - 128
-        'xlm-mlm-100-1280',                                     # (  ) - 128
-        'xlm-roberta-base',                                     # (  ) - 128
-        'xlm-roberta-large',                                    # (  ) - 128
-        'joeddav/xlm-roberta-large-xnli',                       # (  ) - 128
-        'tf_models/neuralmind/bert-large-portuguese-cased',     # (  ) - 128
+        # 'bert-large-cased',                                     # (OK) - 64
+        # 'bert-large-uncased',                                   # (OK) - 64
+        # 'bert-large-uncased-whole-word-masking',                # (OK) - 64
+        # 'bert-large-cased-whole-word-masking',                  # (OK) - 64
+        # 'roberta-large',                                        # (OK) - 64
+        # 'xlm-mlm-17-1280',                                      # (OK) - 64
+        # 'xlm-mlm-100-1280',                                     # (OK) - 64
+        # 'xlm-roberta-base',                                     # (OK) - 64
+        # 'xlm-roberta-large',                                    # (OK) - 64
+        # 'joeddav/xlm-roberta-large-xnli',                       # (OK) - 64
+        # 'tf_models/neuralmind/bert-large-portuguese-cased',     # (OK) - 64
     ],
 
     save_dir='results/model_selection',
